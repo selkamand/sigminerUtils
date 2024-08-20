d8e667544a12f050855fd791d2016b9c09185534
Sam El-Kamand <sam.elkamand@gmail.com>
2024-08-20 13:26:05 +1000

    feat: added file for testing no-doublet samples

--- inst/test.R
+++ inst/test.R
@@ -1,7 +1,14 @@
 devtools::load_all();
 noindel = system.file("test.noindels.maf", package = "sigminerUtils")
+nodoublets = system.file("test.nodoublets.maf", package = "sigminerUtils")
 
 sig_analyse_mutations(
   maf = noindel,
   ref = "hg19"
 )
+
+sig_analyse_mutations(
+  maf = nodoublets,
+  ref = "hg19"
+)
+
